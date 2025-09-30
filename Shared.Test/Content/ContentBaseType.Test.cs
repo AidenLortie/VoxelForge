62f67e85f535b256d11bde258f4c94d2f1d4253a
AidenLortie <146017618+AidenLortie@users.noreply.github.com>
2025-09-30 12:18:28 -0400

    Shared Tests

---
+++ Shared.Test/Content/ContentBaseType.Test.cs
@@ -0,0 +1,82 @@
+﻿using VoxelForge.Shared.Content;
+using VoxelForge.Shared.Registry;
+using Xunit;
+
+namespace Shared.Test.Content;
+
+// Test classes to instantiate abstract classes
+public class TestBlock : Block
+{
+    public TestBlock(string id) : base(id) { }
+}
+
+public class TestItem : Item
+{
+    public TestItem(string id) : base(id) { }
+}
+
+public class TestEntity : Entity
+{
+    public TestEntity(string id) : base(id) { }
+}
+
+public class ContentBaseTypeTest
+{
+    // Test to ensure that Block, Item, and Entity are abstract classes
+    [Fact]
+    public void Block_ShouldBeAbstract()
+    {
+        // Assert
+        Assert.True(typeof(Block).IsAbstract);
+    }
+    
+    [Fact]
+    public void Item_ShouldBeAbstract()
+    {
+        // Assert
+        Assert.True(typeof(Item).IsAbstract);
+    }
+
+    [Fact]
+    public void Entity_ShouldBeAbstract()
+    {
+        // Assert
+        Assert.True(typeof(Entity).IsAbstract);
+    }
+
+    // Test to ensure that subclasses have Id property and it works correctly
+    [Fact]
+    public void SubClassHasIdProperty()
+    {
+        // Arrange
+        var testBlock = new TestBlock("test_block");
+        var testItem = new TestItem("test_item");
+        var testEntity = new TestEntity("test_entity");
+
+        // Act & Assert
+        Assert.Equal("test_block", testBlock.Id);
+        Assert.Equal("test_item", testItem.Id);
+        Assert.Equal("test_entity", testEntity.Id);
+    }
+
+    // Test to ensure that subclasses can be registered and retrieved from Registry<T>
+    [Fact]
+    public void SubClassWorksInRegistry()
+    {
+        var blockRegistry = new Registry<Block>();
+        var itemRegistry = new Registry<Item>();
+        var entityRegistry = new Registry<Entity>();
+        
+        var testBlock = new TestBlock("test_block");
+        var testItem = new TestItem("test_item");
+        var testEntity = new TestEntity("test_entity");
+        
+        blockRegistry.Register(testBlock.Id, testBlock);
+        itemRegistry.Register(testItem.Id, testItem);
+        entityRegistry.Register(testEntity.Id, testEntity);
+        
+        Assert.Equal(testBlock, blockRegistry.Get("test_block"));
+        Assert.Equal(testItem, itemRegistry.Get("test_item"));
+        Assert.Equal(testEntity, entityRegistry.Get("test_entity"));
+    }
+}
---
+++ Shared.Test/Registry/Registry.Test.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using VoxelForge.Shared.Registry;
+using Xunit;
+
+namespace Shared.Test.Registry;
+
+class TestObject
+{
+    public string Name { get; set; }
+    public int Value { get; set; }
+
+    public TestObject(string name, int value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
+
+public class RegistryTest
+{
+    
+    
+    // Test registering and retrieving objects
+    [Fact]
+    public void RegisterAndRetrieve()
+    {
+        var objRegistry = new Registry<TestObject>();
+        
+        var obj1 = new TestObject("Object1", 10);
+        var obj2 = new TestObject("Object2", 20);
+        
+        objRegistry.Register("obj1", obj1);
+        objRegistry.Register("obj2", obj2);
+        
+        var retrievedObj1 = objRegistry.Get("obj1");
+        var retrievedObj2 = objRegistry.Get("obj2");
+        
+        Assert.NotNull(retrievedObj1);
+        Assert.NotNull(retrievedObj2);
+        
+        Assert.Equal(obj1, retrievedObj1);
+        Assert.Equal(obj2, retrievedObj2);
+    }
+
+    // Test retrieving a non-existent object
+    [Fact]
+    public void RetrieveNonExistent()
+    {
+        var objRegistry = new Registry<TestObject>();
+        var retrievedObj = objRegistry.Get("nonexistent");
+        Assert.Null(retrievedObj);
+    }
+
+    // Test duplicate registration
+    [Fact]
+    public void DuplicateRegistration()
+    {
+        var objRegistry = new Registry<TestObject>();
+        
+        var obj1 = new TestObject("Object1", 10);
+        var obj2 = new TestObject("Object2", 20);
+        
+        objRegistry.Register("obj", obj1);
+        objRegistry.Register("obj", obj2); // Duplicate registration
+        
+        var retrievedObj = objRegistry.Get("obj");
+        
+        Assert.NotNull(retrievedObj);
+        Assert.NotEqual(obj1, retrievedObj); // Should not retrieve the first object
+        Assert.Equal(obj2, retrievedObj); // Should retrieve the second object
+    }
+
+    // Test retrieving all entries
+    [Fact]
+    public void GetAllEntries()
+    {
+        var objRegistry = new Registry<TestObject>();
+        var obj1 = new TestObject("Object1", 10);
+        var obj2 = new TestObject("Object2", 20);
+        objRegistry.Register("obj1", obj1);
+        objRegistry.Register("obj2", obj2);
+        var allEntries = objRegistry.GetAll().ToArray();
+        Assert.Contains(obj1, allEntries);
+        Assert.Contains(obj2, allEntries);
+    }
+
+}
