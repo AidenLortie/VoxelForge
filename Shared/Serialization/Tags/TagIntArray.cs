c4a4247d279fd6431314164177a8264509a84965
AidenLortie <146017618+AidenLortie@users.noreply.github.com>
2025-10-08 11:44:51 -0400

    Changes to tag classes for proper serialization / deserialization

---
+++ Shared/Serialization/Tags/TagIntArray.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace VoxelForge.Shared.Serialization.Tags;
+
+public class TagIntArray : Tag
+{
+    public int[] Value { get; set; }
+    public TagIntArray(int[] value, string? name = null)
+    {
+        Value = value;
+        Name = name;
+    }
+
+    public TagIntArray(string? name = null)
+    {
+        Value = new int[1];
+        Name = name;
+    }
+    public override TagType Type => TagType.IntArray;
+
+    public override void Write(BinaryWriter writer)
+    {
+        if (Name != null)
+        {
+            var nameBytes = Encoding.UTF8.GetBytes(Name);
+            writer.Write(nameBytes.Length);
+            writer.Write(nameBytes);
+        }
+        else
+        {
+            writer.Write(0);
+        }
+        writer.Write(Value.Length);
+        foreach (int i in Value)
+        {
+            writer.Write(i);
+        }
+    }
+
+    public override void Read(BinaryReader reader)
+    {
+        int nameLen = reader.ReadInt32();
+        if (nameLen > 0)
+            Name = Encoding.UTF8.GetString(reader.ReadBytes(nameLen));
+        else
+            Name = null;
+        int length = reader.ReadInt32();
+        Value = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            Value[i] = reader.ReadInt32();
+        }
+    }
+    
+    public override string ToString() => $"{{ {Name ?? "(unnamed)"} : {Value.Length} bytes }}";
+}
---
+++ Shared/Serialization/Tags/TagLongArray.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace VoxelForge.Shared.Serialization.Tags;
+
+public class TagLongArray : Tag
+{
+    public long[] Value { get; set; }
+    public TagLongArray(long[] value, string? name = null)
+    {
+        Value = value;
+        Name = name;
+    }
+
+    public TagLongArray(string? name = null)
+    {
+        Value = new long[1];
+        Name = name;
+    }
+    public override TagType Type => TagType.LongArray;
+
+    public override void Write(BinaryWriter writer)
+    {
+        if (Name != null)
+        {
+            var nameBytes = Encoding.UTF8.GetBytes(Name);
+            writer.Write(nameBytes.Length);
+            writer.Write(nameBytes);
+        }
+        else
+        {
+            writer.Write(0);
+        }
+        writer.Write(Value.Length);
+        foreach (long i in Value)
+        {
+            writer.Write(i);
+        }
+    }
+
+    public override void Read(BinaryReader reader)
+    {
+        int nameLen = reader.ReadInt32();
+        if (nameLen > 0)
+            Name = Encoding.UTF8.GetString(reader.ReadBytes(nameLen));
+        else
+            Name = null;
+        int length = reader.ReadInt32();
+        Value = new long[length];
+        for (int i = 0; i < length; i++)
+        {
+            Value[i] = reader.ReadInt64();
+        }
+    }
+    
+    public override string ToString() => $"{{ {Name ?? "(unnamed)"} : {Value.Length} bytes }}";
+}
